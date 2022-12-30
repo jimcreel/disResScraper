fbae315ac2e3c5ae79bd4cccd50c6db7557da7da
jimcreel <jim.creel@gmail.com>
2022-12-29 16:12:33 -0800

    modified header to work on remote server

--- importrequests.py
+++ importrequests.py
@@ -18,7 +18,7 @@ url='https://disneyland.disney.go.com/passes/blockout-dates/api/get-availability
 
 #open the site
 print('opening the reservation site...')
-resp=requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"})
+resp=requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"})
 print(resp)
 dates_dict = resp.text
 parse_json = json.loads(dates_dict)
